2dd696228f83ccd7ed7a78c11727264fff2b4769
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-02-11 15:17:04 +0000

    Initial Commit

---
+++ TokTok/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TokTok
+//
+//  Created by Pedro Miguel Pereira on 11/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TokTok/TokTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokTokApp.swift
+//  TokTok
+//
+//  Created by Pedro Miguel Pereira on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TokTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
