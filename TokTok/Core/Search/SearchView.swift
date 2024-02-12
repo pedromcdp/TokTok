62c0a61a3240cbc255635818dcf373a643ed0b11
Pedro Miguel Pereira <pedromcdp@gmail.com>
2024-02-12 17:40:04 +0000

    Project Setup & and initial Views configuration

---
+++ TokTok/Core/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  TokTok
+//
+//  Created by Pedro Miguel Pereira on 11/02/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ TokTok/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TokTok
+//
+//  Created by Pedro Miguel Pereira on 11/02/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
